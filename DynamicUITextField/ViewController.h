167e98e923caab3cb1f67c076fe911bb0f340277
Mark Wong <markwong.digital@gmail.com>
2022-05-25 14:07:13 -0700

    Initial commit

--- DynamicUITextField/ViewController.h
+++ DynamicUITextField/ViewController.h
@@ -7,8 +7,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
+@property (strong, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
--- DynamicUITextField/ViewController.m
+++ DynamicUITextField/ViewController.m
@@ -7,6 +7,24 @@
 
 #import "ViewController.h"
 
+typedef NS_ENUM(int, FontColor) {
+  Blue,
+  Brown,
+  Cyan,
+  Green,
+  Indigo,
+  Mint,
+  Orange,
+  Pink,
+  Purple,
+  Red,
+  Teal,
+  Yellow,
+  Gray,
+  Black,
+  White
+};
+
 @interface ViewController ()
 
 @end
@@ -18,5 +36,24 @@
   // Do any additional setup after loading the view.
 }
 
+- (IBAction)textEditingChanged: (UITextField *)sender {
+  NSArray *words = [[sender text] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+  
+  for (NSString *word in words) {
+    NSLog(@"%@", word);
+    
+    NSRegularExpression *colorRegex = [NSRegularExpression regularExpressionWithPattern:@"^(blue|brown|cyan|green|indigo|mint|orange|pink|purple|red|teal|yellow|gray|black|white)$" options:NSRegularExpressionCaseInsensitive error:nil];
+    
+    if ([colorRegex numberOfMatchesInString:word options:0 range:NSMakeRange(0, [word length])] >= 1) {
+      NSString *lowercased = [word lowercaseString];
+      NSDictionary *attributes = [[NSDictionary alloc] initWithObjectsAndKeys:NSForegroundColorAttributeName, [UIColor colorNamed:word], nil];
+      NSAttributedString *newString = [[NSAttributedString alloc] initWithString:word attributes:attributes];
+      
+      [sender shouldChangeTextInRange:<#(nonnull UITextRange *)#> replacementText:<#(nonnull NSString *)#>]
+    }
+  }
+}
+
+//- (UIColor *)getTextColor: ()
 
 @end
